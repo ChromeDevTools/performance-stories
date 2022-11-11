6f91f0287f80adfbc9422fc5edf2db6aca0bc5cd
Jack Franklin <jacktfranklin@chromium.org>
2022-11-11 09:58:24 +0000

    Fix placeholder image

--- lcp-critical-path/load.js
+++ lcp-critical-path/load.js
@@ -1,5 +1,5 @@
 export function insertImage() {
-  const img = 'https://placedog.net/500/500';
+  const img = 'https://via.placeholder.com/500/500';
   const image = new Image();
   image.src = img;
   document.getElementById('app').appendChild(image);

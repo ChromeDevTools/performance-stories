6e54395511cc072657374933b04096a0e2fd613b
Jack Franklin <jacktfranklin@google.com>
2023-04-13 10:11:49 +0200

    Add nested interactions example (#13)

---
+++ nested-interactions/app.js
@@ -0,0 +1,13 @@
+function fibonacci(num) {
+  if (num <= 1) return 1;
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+const input = document.querySelector('#text');
+
+input.addEventListener('keydown', (e) => {
+  e.preventDefault()
+  fibonacci(30);
+  input.value += e.key
+})

81948f1ba10202a1d426567f35beab5151d91b5a
Jack Franklin <jacktfranklin@google.com>
2023-07-19 09:59:42 +0100

    Add idle-callback-time example

---
+++ idle-callback-time/app.js
@@ -0,0 +1,14 @@
+function fibonacci(num) {
+  if (num <= 1) return 1;
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+function run() {
+  requestIdleCallback(() => {
+  fibonacci(35)
+    document.body.style.background = 'lightblue';
+  })
+}
+
+run()

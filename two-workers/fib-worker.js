b8d1cc5f4bf11866143152157bfc61225c8c0e5d
Jack Franklin <jacktfranklin@google.com>
2023-07-27 10:03:18 +0200

    Add workers example

---
+++ two-workers/fib-worker.js
@@ -0,0 +1,10 @@
+function fibonacci(num) {
+  if (num <= 1) return 1;
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+self.onmessage = function(event) {
+    const result = fibonacci(event.data.value);
+    self.postMessage(result)
+}

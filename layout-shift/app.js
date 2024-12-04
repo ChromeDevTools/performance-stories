9b85b65ac0500808b7f86eea14b76ea35451291b
Jack Franklin <jacktfranklin@google.com>
2024-12-04 09:06:11 +0000

    Make the layout shifts example much more extreme

--- layout-shift/app.js
+++ layout-shift/app.js
@@ -1,3 +1,24 @@
 setTimeout(function () {
   document.querySelector('#red').style.height = 500;
 }, 500);
+setTimeout(function () {
+  document.querySelector('#red').style.height = 700;
+}, 700);
+setTimeout(function () {
+  document.querySelector('#red').style.height = 200;
+}, 900);
+setTimeout(function () {
+  document.querySelector('#red').style.height = 500;
+}, 1200);
+setTimeout(function () {
+  document.querySelector('#red').style.height = 800;
+}, 1600);
+setTimeout(function () {
+  document.querySelector('#red').style.height = 100;
+}, 2000);
+setTimeout(function () {
+  document.querySelector('#red').style.height = 1000;
+}, 2200);
+setTimeout(function () {
+  document.querySelector('#red').style.height = 100;
+}, 2600);

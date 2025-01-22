2d221122a18249c6e302e3e122856ce8fdb37b76
Jack Franklin <jacktfranklin@google.com>
2025-01-22 15:08:46 +0000

    Add delay to LCP Critical path example (#40)

--- lcp-critical-path/app.js
+++ lcp-critical-path/app.js
@@ -1,3 +1,9 @@
 import('./load.js').then((module) => {
+  sleepSync(1_500); // artificial delay
   module.insertImage();
 });
+
+function sleepSync(ms) {
+  const start = Date.now();
+  while (Date.now() - start < ms);
+}

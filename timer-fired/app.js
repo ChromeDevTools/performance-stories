674d9adeea6df1e0f0ade660d0746d4df103b3cb
Jack Franklin <jacktfranklin@google.com>
2025-07-16 08:38:22 +0100

    Add artifical delay to timer fired funcs

--- timer-fired/app.js
+++ timer-fired/app.js
@@ -1,11 +1,20 @@
 function helloWorld() {
+  sleepSync(100);
   console.log('hello, world!');
 }
 
 setTimeout(helloWorld, 500);
 
 setTimeout(() => {
+  sleepSync(100);
   console.log('anonymous, hello world!');
 }, 1000);
 
 setInterval(helloWorld, 1500);
+
+function sleepSync(milliseconds) {
+  const start = Date.now();
+  while (Date.now() - start < milliseconds) {
+    // Busy-wait, blocks the main thread
+  }
+}

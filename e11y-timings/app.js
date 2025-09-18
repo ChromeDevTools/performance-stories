b5bc91c8c2b44bd8d03cae44c9c59e811d857724
Jack Franklin <jacktfranklin@google.com>
2025-09-18 08:46:11 +0100

    Add basic DevTools e11y perf API example

---
+++ e11y-timings/app.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+  const now = performance.now();
+  performance.measure('Update', {
+    start: now - 1000,
+    end: now - 500,
+    detail: {
+      devtools: {
+        track: 'My Track',
+        trackGroup: 'My Track Group',
+      },
+    },
+  });
+  performance.measure('Update', {
+    start: now - 500,
+    end: now,
+    detail: {
+      devtools: {
+        track: 'My Track',
+        trackGroup: 'My Track Group',
+      },
+    },
+  });
+
+  performance.measure('Some other event', {
+    start: now - 700,
+    end: now + 500,
+    detail: {
+      devtools: {
+        track: 'My Track',
+        trackGroup: 'My Track Group',
+      },
+    },
+  });
+}, 2000);
